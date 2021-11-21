8b7e3c99bc6fdcb15f45008e8472f8b070323c5f
bluncasu <luncasub11@gmail.com>
2021-11-21 13:57:39 +0200

    integrate with blockchain
    TODO: change navbar to use Metamask function in order to reduce duplicate code

---
+++ src/components/Metamask/Metamask.jsx
@@ -0,0 +1,75 @@
+import React, { useState } from 'react'
+import Web3 from 'web3';
+import abi from './abi.json';
+
+const { ethereum } = window;
+const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
+const contract_address = "0xFF1233536Cd163FF8970Da4462cc7f4E01e4f0d6";
+const psn = new web3.eth.Contract(abi, contract_address);
+var current_account = undefined;
+
+const isMetaMaskInstalled = () => {
+  return Boolean(ethereum && ethereum.isMetaMask);
+};
+
+const init = (state) => {
+  const wrapp = () => {
+    if(isMetaMaskInstalled()){
+      web3.eth.getAccounts(function(err, accounts){
+          if (err != null) console.error("An error occurred: "+err);
+          else if (accounts.length == 0) console.log("User is not logged in to MetaMask");
+          else {
+            state(s => true)
+            current_account = accounts[0];
+            console.log("User is logged in to MetaMask");
+          }
+      });
+    }
+  }
+
+  return wrapp;
+}
+
+const handleMetaMask = (state) => {
+    const wrapp = () => {
+      console.log("Handle MetaMask Connection");
+      if(isMetaMaskInstalled()){
+        ethereum.request({ method: 'eth_requestAccounts' }).then((result) => {
+          console.log("Connected as" + result[0]);
+          current_account = result[0];
+          state(s => true)
+        });
+      }
+    }
+
+    return wrapp;
+};
+
+const mint = (num) => {
+  const wrapp = () => {
+    web3.eth.getTransactionCount(current_account, 'latest').then(function(nonce) {
+      console.log("Nonce " + nonce)
+      let data = psn.methods.mint(num).encodeABI();
+      let transactionParameters = {
+          "from": current_account,
+          "nonce": web3.utils.toHex(nonce),
+          "gasPrice": web3.utils.toHex(5 * 10e8),
+          "gasLimit": web3.utils.toHex(330000),
+          "to": contract_address,
+          "value": web3.utils.toHex(num*6e16),
+          "data": data,
+      }
+
+      const txHash = ethereum.request({
+        method: 'eth_sendTransaction',
+        params: [transactionParameters],
+      });
+
+      console.log(txHash)
+    })
+  }
+
+  return wrapp
+}
+
+export {isMetaMaskInstalled, init, handleMetaMask, mint}
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,27 +5,54 @@ import "./Navbar.css"
 import NavItems from './NavItems'
 import SocialMedia from './SocialMedia'
 import Metamask from "../../assets/logos/metamask-fox.svg"
-
+import Web3 from 'web3';
 
 const Navbar = ({ setPage }) => {
 
     const [metaMaskConnected, setMetaMaskConnected] = useState(false)
     const [isShown, setIsShown] = useState(false);
+    const { ethereum } = window;
+    const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
+
+    const init = () => {
+      if(isMetaMaskInstalled()){
+        web3.eth.getAccounts(function(err, accounts){
+            if (err != null) console.error("An error occurred: "+err);
+            else if (accounts.length == 0) console.log("User is not logged in to MetaMask");
+            else {
+              setMetaMaskConnected(state => true);
+              console.log("User is logged in to MetaMask");
+            }
+        });
+      }
+    }
+
+    const isMetaMaskInstalled = () => {
+      return Boolean(ethereum && ethereum.isMetaMask);
+    };
 
     const handleMetaMask = () => {
         console.log("Handle MetaMask Connection - NavBar");
+        if(isMetaMaskInstalled()){
+          ethereum.request({ method: 'eth_requestAccounts' }).then((result) => {
+            console.log("Connected as" + result[0]);
+            setMetaMaskConnected(state => true);
+          });
+        }
     };
+
+    init();
 
     return (
         <nav className="navbar" onMouseEnter={() => setIsShown(true)} onMouseLeave={() => setIsShown(false)}>
             <div className="nav-sections">
                 <div className='nav-links'>
-                    {!metaMaskConnected && 
+                    {!metaMaskConnected &&
                         <li>
                             <img src={Metamask} alt='metamask-logo' className='nav-icon display' />
-                            <button 
+                            <button
                             className="cybr-btn display-none"
-                            onClick={handleMetaMask()}
+                            onClick={handleMetaMask}
                             >
                                 Connect<span aria-hidden>_</span>
                                 <span aria-hidden className="cybr-btn__glitch">Connect_</span>
@@ -35,7 +62,7 @@ const Navbar = ({ setPage }) => {
                     {NavItems.navitems.map((navitem) => (
                         <li id={navitem.name}>
                             <FontAwesomeIcon icon={navitem.icon} className='nav-icon display'></FontAwesomeIcon>
-                            <button 
+                            <button
                             className="cybr-btn display-none"
                             onClick={() => setPage(navitem.id)}
                             >
